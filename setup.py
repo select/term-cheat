93fd8584de500abb80045cb661342b522c4332fa
Fa!lko <falko@webpgr.com>
2019-02-03 18:00:40 +0100

    setup file for pypi

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='TermCheat',
+      version='0.1.0',
+      description='Collect and find termial commands.',
+      author='@select@github.com',
+      author_email='falko@webpgr.com',
+      url='https://github.com/select/term-cheat',
+      packages=['app'],
+      install_requires=['urwid', 'fuzzywuzzy', 'yaml', 'logging']
+     )
+
+from distutils.core import setup
+
