e8a774495913764a204198938af3183853ab8afb
Fa!lko <falko@webpgr.com>
2019-02-06 02:49:40 +0100

    clean up after makin snap kind of work

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@
 
 from distutils.core import setup
 
+
 setup(name='TermCheat',
-      version='0.1.4',
+      version='0.1.8',
       description='Collect and find termial commands.',
       author='@select@github.com',
       author_email='falko@webpgr.com',
       url='https://github.com/select/term-cheat',
       packages=['termcheat'],
       package_data={'termcheat': ['*.yaml']},
-      entry_points={
-          'console_scripts': ['term-cheat-app = termcheat.lib:run']
-      },
+      # entry_points={
+      #     'console_scripts': ['term-cheat-app = termcheat.app:run']
+      # },
       # scripts=["bin/term-cheat-app"],
-      install_requires=['urwid', 'fuzzywuzzy', 'pyyaml', 'appdirs', 'python-Levenshtein']
+      # install_requires=['urwid', 'fuzzywuzzy', 'pyyaml', 'appdirs', 'python-Levenshtein']
       )
--- termcheat/__main__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-from . import lib
-
-lib.run()
